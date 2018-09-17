423b4d2278b6daf4992fd976c0cf9e429f21e1c1
YSG12 <6179466852@qq.com>
2018-09-17 23:17:57 +0800

    添加示例页

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/include', function(req, res, next) {
+    res.render('include', { title: 'include' });
+});
+
+/* GET home page. */
+router.get('/extend', function(req, res, next) {
+    res.render('extend', { title: 'extend' });
+});
+
+
 module.exports = router;
